5322a3fdc06fa1c536f16c24bbec92d997a41c49
Neha Nehru <neha_nehru@hotmail.com>
2019-11-04 23:43:36 -0800

    Adding mysql files

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,16 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS personadb;
+CREATE DATABASE personadb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE personaDB;
+
+CREATE TABLE persona(
+  id INT NOT NULL AUTO_INCREMENT,
+  characterName VARCHAR(45) NULL,
+  powerstats VARCHAR(60) NULL,
+  biography VARCHAR(45) NULL,
+  appearance VARCHAR(45) NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM persona;
+
---
+++ models/seeds.sql
@@ -0,0 +1,16 @@
+USE personaDB;
+
+INSERT INTO persona(characterName, powerstats, biography, appearnce)
+VALUES ("Batman","Intelligence:81, Strength:40, Speed:29","Alter-ego: No alter ego found, Place of Birth:Gotham City - 25th Century, Publisher: DC Comics", "Gender:Male, Race:Human");
+
+INSERT INTO persona(characterName, powerstats, biography, appearnce)
+VALUES ("Ethan Hunt"," tbd","tbd", "tbd");
+
+INSERT INTO persona(characterName, powerstats, biography, appearnce)
+VALUES ("Nick Fury"," tbd","tbd", "tbd");
+
+INSERT INTO persona(characterName, powerstats, biography, appearnce)
+VALUES ("Evil Deadpool"," tbd","tbd", "tbd");
+
+INSERT INTO persona(characterName, powerstats, biography, appearnce)
+VALUES ("Falcon"," tbd","tbd", "tbd");
