fb38727a628e6680a43f0e066b294d84be794f48
Emilio Valladares <vallae.wumbo@gmail.com>
2019-11-05 19:42:33 -0800

    Corrected spelling in seed to avoid error.

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,16 +1,16 @@
 USE personaDB;
 
-INSERT INTO persona(characterName, powerstats, biography, appearnce)
+INSERT INTO persona(characterName, powerstats, biography, appearance)
 VALUES ("Batman","Intelligence:81, Strength:40, Speed:29","Alter-ego: No alter ego found, Place of Birth:Gotham City - 25th Century, Publisher: DC Comics", "Gender:Male, Race:Human");
 
-INSERT INTO persona(characterName, powerstats, biography, appearnce)
+INSERT INTO persona(characterName, powerstats, biography, appearance)
 VALUES ("Ethan Hunt"," tbd","tbd", "tbd");
 
-INSERT INTO persona(characterName, powerstats, biography, appearnce)
+INSERT INTO persona(characterName, powerstats, biography, appearance)
 VALUES ("Nick Fury"," tbd","tbd", "tbd");
 
-INSERT INTO persona(characterName, powerstats, biography, appearnce)
+INSERT INTO persona(characterName, powerstats, biography, appearance)
 VALUES ("Evil Deadpool"," tbd","tbd", "tbd");
 
-INSERT INTO persona(characterName, powerstats, biography, appearnce)
+INSERT INTO persona(characterName, powerstats, biography, appearance)
 VALUES ("Falcon"," tbd","tbd", "tbd");
