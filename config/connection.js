53b68d272ccbbfc360e7e558e6cf740f030ae00f
Candice Chin <chincandice2007@gmail.com>
2019-11-06 20:04:40 -0800

    added orm and connection js files

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+//var dotenv = require('dotenv').config();
+
+//Inside the connection.js file, setup the code to connect Node to MySQL.
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "personadb"
+    });
+};  
+
+//Making connection
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+//Export the connection.
+module.exports = connection;
