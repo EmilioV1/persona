7d2b3eb6b0d0e8df2efcf34d14471ebd137bf6f4
Neha Nehru <neha_nehru@hotmail.com>
2019-11-15 12:58:25 -0800

    made chanches to schema and superheros

--- models/schema.sql
+++ models/schema.sql
@@ -3,17 +3,5 @@ CREATE DATABASE personadb;
 
 USE personadb;
 
-CREATE TABLE persona(
-  name VARCHAR(45) NULL,
-  intelligence INTEGER(60) NULL,
-  speed INTEGER(45) NULL,
-  power INTEGER(45) NULL,
-  placeofbirth VARCHAR(45) NULL,
-  publisher VARCHAR(45) NULL,
-  gender VARCHAR(45) NULL,
-  race VARCHAR(45) NULL,
-  image BLOB(45) NULL,
-);
-
 SELECT * FROM personadb;
 
--- models/superheros.js
+++ models/superheros.js
@@ -1,6 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   var Persona = sequelize.define("Persona", {
     name: DataTypes.STRING,
+    fullname: DataTypes.STRING,
     intelligence: DataTypes.INTEGER,
     speed: DataTypes.INTEGER,
     power: DataTypes.INTEGER,
