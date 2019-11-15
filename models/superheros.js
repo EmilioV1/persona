4f248f72a3c2bfa5fbad405ff6a17c28b0e4f2cf
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-11-15 12:50:52 -0800

    added more colums for persona table

--- models/superheros.js
+++ models/superheros.js
@@ -1,14 +1,21 @@
 module.exports = function(sequelize, DataTypes) {
   var Persona = sequelize.define("Persona", {
     name: DataTypes.STRING,
-    intelligence: DataTypes.INTEGER,
-    speed: DataTypes.INTEGER,
-    power: DataTypes.INTEGER,
-    placeofbirth: DataTypes.STRING,
+    fullname: DataTypes.STRING,
+    appearance: DataTypes.STRING,
     publisher: DataTypes.STRING,
-    gender: DataTypes.STRING,
+    alignment: DataTypes.STRING,
     race: DataTypes.STRING,
-    image: DataTypes.STRING
+    placeofbirth: DataTypeys.STRING,
+    gender: DataTypes.STRING,
+    height: DataTypes.STRING,
+    weight: DataTypes.STRING,
+    intelligence: DataTypes.INTEGER,
+    strength: DataTypes.INTEGER,
+    speed: DataTypes.INTEGER,
+    durability: DataTypes.INTEGER,
+    power: DataTypes.INTEGER,
+    combat: DataTypes.INTEGER
   });
   return Persona;
 };
