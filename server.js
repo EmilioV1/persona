a1acf5380ef5cca25f7d4bb5d678cdad09fd71a9
Emilio Valladares <vallae.wumbo@gmail.com>
2019-11-16 00:23:40 -0800

    Initializing branch to add working design.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+
+
 var express = require("express");
 var exphbs = require("express-handlebars");
 
@@ -10,7 +12,8 @@ var PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static("public"));
 
 // Handlebars
 app.engine(
